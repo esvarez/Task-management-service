2a6a05489936b4fbdd2cb91f7a66760511f9b4d0
Erick Suarez <ericksuarezdev@gmail.com>
2020-08-04 01:02:43 -0500

    Clean files and routes

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x /tmp/deploy/build/libs/pokemon-battle-0.0.1-SNAPSHOT.jar
-chmod +x /tmp/deploy/scripts/start_server.sh
-chmod +x /tmp/deploy/scripts/stop_server.sh
+chmod +x /tmp/deploy/task-management-service-0.0.1-SNAPSHOT.jar
+chmod +x /tmp/deploy/start_server.sh
+chmod +x /tmp/deploy/stop_server.sh
 
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar -Dspring.profiles.active=dev /tmp/deploy/build/libs/pokemon-battle-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar -Dspring.profiles.active=dev /tmp/deploy/task-management-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 
